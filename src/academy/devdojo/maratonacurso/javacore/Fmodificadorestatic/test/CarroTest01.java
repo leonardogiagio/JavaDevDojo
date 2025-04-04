34fe54d6c5d77017d6e3173e0ca922afa9a97663
Leonardo Giagio <leonardogiagio@gmail.com>
2025-04-03 23:00:31 -0300

    add: POO / modificador static

---
+++ src/academy/devdojo/maratonacurso/javacore/Fmodificadorestatic/test/CarroTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonacurso.javacore.Fmodificadorestatic.test;
+
+import academy.devdojo.maratonacurso.javacore.Fmodificadorestatic.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro c1 = new Carro("BMW", 280);
+        Carro c2 = new Carro("Mercedes", 275);
+        Carro c3 = new Carro("Audi", 290);
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+
+        c1.setVelocidadeLimite(180);
+
+        c1.imprime();
+        c2.imprime();
+        c3.imprime();
+    }
+}
